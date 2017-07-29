5af5cc08794d991fed458e5d1f2c5591a6d1194a
rickmutua <rick.mutua12@gmail.com>
2017-07-29 05:11:15 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,26 @@ $(window).load(function() { //start after HTML, images have loaded
     InfiniteRotator.init();
 
 });
+
+//dropdown base
+$("<select />").appendTo("nav");
+
+//default option "Go to..."
+$("<option />", {
+   "selected": "selected",
+   "value"   : "",
+   "text"    : "Go to..."
+}).appendTo("nav select");
+
+// Populate dropdown with menu items
+$("nav a").each(function() {
+ var el = $(this);
+ $("<option />", {
+     "value"   : el.attr("href"),
+     "text"    : el.text()
+ }).appendTo("nav select");
+});
+
+$("nav select").change(function() {
+  window.location = $(this).find("option:selected").val();
+});
