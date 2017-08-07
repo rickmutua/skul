411907b830822e8005830cc2f6e0d3b28d25de00
rickmutua <rick.mutua12@gmail.com>
2017-08-07 19:04:39 +0300

    added an enquiry form

--- js/scripts.js
+++ js/scripts.js
@@ -41,25 +41,34 @@ $(window).load(function() { //start after HTML, images have loaded
 
 });
 
-//dropdown base
-$("<select />").appendTo("nav");
+$('document').ready(function(){
+	$('input[type="text"], input[type="email"], textarea').focus(function(){
+		var background = $(this).attr('id');
+		$('#' + background + '-form').addClass('formgroup-active');
+		$('#' + background + '-form').removeClass('formgroup-error');
+	});
+	$('input[type="text"], input[type="email"], textarea').blur(function(){
+		var background = $(this).attr('id');
+		$('#' + background + '-form').removeClass('formgroup-active');
+	});
 
-//default option "Go to..."
-$("<option />", {
-   "selected": "selected",
-   "value"   : "",
-   "text"    : "Go to..."
-}).appendTo("nav select");
+function errorfield(field){
+	$(field).addClass('formgroup-error');
+	console.log(field);
+}
 
-// Populate dropdown with menu items
-$("nav a").each(function() {
- var el = $(this);
- $("<option />", {
-     "value"   : el.attr("href"),
-     "text"    : el.text()
- }).appendTo("nav select");
+$("#waterform").submit(function() {
+	var stopsubmit = false;
+
+if($('#name').val() == "") {
+	errorfield('#name-form');
+	stopsubmit=true;
+}
+if($('#email').val() == "") {
+	errorfield('#email-form');
+	stopsubmit=true;
+}
+  if(stopsubmit) return false;
 });
 
-$("nav select").change(function() {
-  window.location = $(this).find("option:selected").val();
 });
